d49dac030e2ae2caa77e7a171858ba247dbb4c8d
Paulina <hryciuk.paulina1@gmail.com>
2023-12-07 13:45:46 +0100

    excercises

---
+++ cw po zajeciach 3,4.py
@@ -0,0 +1,48 @@
+# b = 1234564.569
+# print(f"hahha {b:.2f} and {b:,f}")
+# a = 10
+# klucze = {"A", "B"}
+# dict1 = dict()
+# dict1 = dict.fromkeys(klucze)
+# print(dict1)
+# klucze = {"a","b"}
+# wartosci = ['app','bone']
+# dict_my = dict.fromkeys(klucze,wartosci)
+# print(dict_my)
+# print(dict_my.get("b"))
+# print(dict_my.get("c","nie ma"))
+# dict_my.update([("M","T"),("mama","tata")])
+# print(dict_my)
+# import random
+# lista = [ 1,2,3,4,5,6]
+# print(random.choices(lista,k = 2))
+# print(random.sample(lista,k =2))
+
+# a = input("alfobet :")
+# match a:
+#     case "a":
+#         print('ala')
+#     case 'b':
+#         print("bob")
+#     case _:
+#         print("ajsuadhk")
+
+# for n in enumerate(a, start=10):
+#     print(n)
+# for n,l in enumerate(a, start=10):
+#     print(n,l)
+
+
+# lista1 = ["O","A"]
+# lista2 = ["ola", "ala"]
+# for i in zip(lista1, lista2):
+#     print(i)
+# for i,j in zip(lista1, lista2):
+#     print(i,j)
+
+# for i in dict_my.keys():
+#     print(i)
+# for i in dict_my:
+#     print(i)
+# for i in dict_my.values():
+#     print(i)
