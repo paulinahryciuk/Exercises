170a50f012f12bb7c8191fc40964ab56e1ad0050
Paulina <hryciuk.paulina1@gmail.com>
2023-12-29 07:30:46 +0100

    excercises

--- cw po zaj 5,6.py
+++ cw po zaj 5,6.py
@@ -18,3 +18,26 @@
 #
 # lista2 = list(range(1,101))
 # print(list(filter(lambda x: x%3 == 0 and x%4 != 0, lista2)))
+
+#
+# lista = [2,3,2,5,5,6,6,7]
+# from functools import reduce
+# print(reduce(lambda x,y : y-x ,lista))
+
+# x = int(input("Podaj liczbe calkowita: "))
+
+# def sumuj_mniejsze(x):
+#     calkowite = list(range(1, 10))
+#     suma =0
+#     for c in calkowite:
+#         if c <x:
+#             return c + suma
+#     print(suma)
+
+def sumuj_mniejsze(x):
+    if x==0:
+        return 0
+    else:
+        return x + sumuj_mniejsze(x-1)
+
+print(sumuj_mniejsze(4))
