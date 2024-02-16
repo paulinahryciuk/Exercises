332827272abd4efe3f49b11c529d4066081c3320
Paulina <hryciuk.paulina1@gmail.com>
2024-02-16 19:30:31 +0100

    excercises

--- cw po 11,12.py
+++ cw po 11,12.py
@@ -1,20 +1,64 @@
-import sqlite3
+# import sqlite3
+#
+# # try:
+# #     polaczenie = sqlite3.connect("baza_ksiazkowa.db")
+# #     kursor = polaczenie.cursor()
+# #     print("baza podlaczona")
+# #
+# #     # tabela = """
+# #     # cre"""
+# #     #
+# #     # kursor.execute(tabela)
+# #     # polaczenie.commit()
+# #
+# # except polaczenie.Error as e:
+# #     print(f"blad {e}")
+# #
+# # finally:
+# #     if polaczenie:
+# #         polaczenie.close()
+# #         print("polacznie zakmniete")
+# #
+# #
+# # from sqlalchemy import create_engine, Column, Integer, String
+# # from sqlalchemy.orm import sessionmaker, declarative_base
+# #
+# # Base = declarative_base()
+# # class User(Base):
+# #     __table_name__ = 'tabela'
+# #
+# # engine = create_engine(baza)
+# # Base.mets
+#
+# class ConMan:
+#     def __init__(self,db_name):
+#         self.db_name = db_name
+#         self.polaczenie = polaczenie
+#
+#     def __enter__(self):
+#         polaczenie = sqlite3.connect(self.db_name)
+#         return self.polaczenie
+#
+#     def __exit__(self, exc_type, exc_val, exc_tb):
+#         if self.polaczenie:
+#             self.polaczenie.commit()
+#             self.polaczenie.close()
+#
+#     with ConMan(db_name) as polaczenie:
+#             kursor = polaczenie.cursor()
+#             kursor.execute()
 
-try:
-    polaczenie = sqlite3.connect("baza_ksiazkowa.db")
-    kursor = polaczenie.cursor()
-    print("baza podlaczona")
 
-    # tabela = """
-    # cre"""
-    #
-    # kursor.execute(tabela)
-    # polaczenie.commit()
+from sqlalchemy import (
+    Column, Integer, String, ForeignKey, create_engine
+)
 
-except polaczenie.Error as e:
-    print(f"blad {e}")
+from sqlalchemy.orm import relationship, sessionmaker, declarative_base
 
-finally:
-    if polaczenie:
-        polaczenie.close()
-        print("polacznie zakmniete")
+Base = declarative_base()
+
+class Persor(Base):
+    __tablename__ = 'osobki'
+    imie  = Column(String, primary_key=True)
+    nazwisko = Column(String)
+    wiek = Column(Integer)
