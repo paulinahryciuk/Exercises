f61a527cf5fe1206f3148367ba9cbbea1b5912b2
Paulina <hryciuk.paulina1@gmail.com>
2024-02-18 06:59:55 +0100

    excercises

--- cw po 11,12.py
+++ cw po 11,12.py
@@ -48,6 +48,53 @@
 #             kursor = polaczenie.cursor()
 #             kursor.execute()
 
+#
+# from sqlalchemy import (
+#     Column, Integer, String, ForeignKey, create_engine
+# )
+#
+# from sqlalchemy.orm import relationship, sessionmaker, declarative_base
+#
+# Base = declarative_base()
+#
+# class Person(Base):
+#     __tablename__ = 'osobki'
+#     id = Column (Integer, primary_key = True)
+#     imie  = Column(String)
+#     nazwisko = Column(String)
+#     wiek = Column(Integer)
+#     miejsce = relationship('Adress',
+#                            back_populates='person',
+#                            cascade='all, delete-orphan')
+#
+# class Adress(Base):
+#     __tablename__ = 'miasta'
+#     id = Column(Integer, primary_key = True)
+#     miasto = Column(String)
+#     person_id = Column(ForeignKey('osobki.id'))
+#     person = relationship('Person',back_populates='miejsce')
+#
+# engine = create_engine('sqlite:///zamieszkanie2.db')
+# Base.metadata.create_all(engine)
+# Session = sessionmaker(bind=engine)
+# session = Session()
+#
+# ala = Person(imie = "Ala",nazwisko = "kkk",wiek = 20)
+# ola = Person(imie = "Ola",nazwisko = "mmm",wiek = 30)
+# ala.miejsce = [Adress(miasto ='krakow')]
+# ola.miejsce = [Adress(miasto ='wawa'),
+#              Adress(miasto = 'lodz')]
+#
+# session.add(ala)
+# session.add(ola)
+# # session.add(ola_adres)
+# # session.add(ala_adres)
+#
+# session.commit()
+#
+# users = session.query(Person.imie).all()
+# print(users)
+
 
 from sqlalchemy import (
     Column, Integer, String, ForeignKey, create_engine
@@ -57,40 +104,49 @@ from sqlalchemy.orm import relationship, sessionmaker, declarative_base
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'osobki'
-    id = Column (Integer, primary_key = True)
-    imie  = Column(String)
-    nazwisko = Column(String)
-    wiek = Column(Integer)
-    miejsce = relationship('Adress',
-                           back_populates='person',
-                           cascade='all, delete-orphan')
 
-class Adress(Base):
-    __tablename__ = 'miasta'
-    id = Column(Integer, primary_key = True)
-    miasto = Column(String)
-    person_id = Column(ForeignKey('osobki.id'))
-    person = relationship('Person',back_populates='miejsce')
+class Owoc(Base):
+    __tablename__ = 'owoce'
+    id = Column(Integer, primary_key= True)
+    nazwa = Column(String)
+    # kolorek = relationship('Typ1', backref='owoc_kolor')
+    # smaczek = relationship(('Typ2', backref= 'owoc_smak'))kolorek = relationship('Typ1', backref='owoc_kolor')
+    kolorek = relationship('Typ1', back_populates='owoc_kolor', cascade='all, delete-orphan')
+    smaczek = relationship('Typ2', back_populates= 'owoc_smak', cascade = 'all, delete-orphan')
 
-engine = create_engine('sqlite:///zamieszkanie2.db')
+
+class Typ1(Base):
+    __tablename__ = 'typ1'
+    id = Column(Integer, primary_key=True)
+    kolor = Column(String)
+    kolorek_id = Column(ForeignKey('owoce.id'))
+    # owoc_kolor = Column(ForeignKey(Owoc, 'kolorek'))
+    owoc_kolor = relationship('Owoc', back_populates='kolorek')
+
+
+class Typ2(Base):
+    __tablename__ = 'typ2'
+    id2 = Column(Integer, primary_key=True)
+    smak = Column(String)
+    # owoc_smak = Column(ForeignKey(Owoc, 'smaczek'))
+    smak_id = Column(ForeignKey('owoce.id'))
+    owoc_smak = relationship('Owoc', back_populates='smaczek')
+
+
+engine = create_engine('sqlite:///owoce.db')
 Base.metadata.create_all(engine)
+pierwszy = Owoc(nazwa='jablko')
+drugi = Owoc(nazwa='banan')
+trzeci = Owoc(nazwa='aronia')
+pierwszy.kolorek = [Typ1(kolor='zielony')]
+drugi.kolorek = [Typ1(kolor='zolty')]
+trzeci.kolorek = [Typ1(kolor='cirmny')]
+pierwszy.smaczek = [Typ2(smak='kwasny')]
+drugi.smaczek = [Typ2(smak='slodki')]
+trzeci.smaczek = [Typ2(smak='cierpki')]
 Session = sessionmaker(bind=engine)
 session = Session()
-
-ala = Person(imie = "Ala",nazwisko = "kkk",wiek = 20)
-ola = Person(imie = "Ola",nazwisko = "mmm",wiek = 30)
-ala.miejsce = [Adress(miasto ='krakow')]
-ola.miejsce = [Adress(miasto ='wawa'),
-             Adress(miasto = 'lodz')]
-
-session.add(ala)
-session.add(ola)
-# session.add(ola_adres)
-# session.add(ala_adres)
-
+session.add(pierwszy)
+session.add(drugi)
+session.add(trzeci)
 session.commit()
-
-users = session.query(Person.imie).all()
-print(users)
