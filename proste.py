7d8ea14ce30e1bf774b2a9e4e7bb36ca56a9e7df
Paulina <hryciuk.paulina1@gmail.com>
2024-07-06 08:08:52 +0200

    excercises

--- proste.py
+++ proste.py
@@ -49,17 +49,52 @@
 # # osoba = list
 # a=list[-1:]index(/n)
 
-text = '''
-3
-Kryszna 67 68 69
-Arjun 70 98 63
-Malika 52 56 60
-Malika
-'''
+# text = '''
+# 3
+# Kryszna 67 68 69
+# Arjun 70 98 63
+# Malika 52 56 60
+# Malika
+# '''
+#
+# text2 = text.replace(" ",",")
+# print(text2)
+# licz_osob = text2[1]
+# print(f"liczba osob to {licz_osob}")
+# # a =text2[::-1].find("\n"")
+# # print(a)
 
-text2 = text.replace(" ",",")
-print(text2)
-licz_osob = text2[1]
-print(f"liczba osob to {licz_osob}")
-# a =text2[::-1].find("\n"")
-# print(a)
+
+# print(type('a'))
+# print(5 * 's')
+# a: int = 5
+# a = ('aoao')
+# b = 1.234
+# print("aaa to %d" %b)
+# print("aaa to %5f" %b)
+# print(f"halo halo tu {b:.8f}")
+# c=100000000000000
+# print(f"to jest liczba {c:,}")
+# d='ola ma kota'
+# print(d.split())
+
+
+# liczby3 = [3, 8, 5, 12, 1]
+# liczby3.insert(3,0)
+# print(liczby3)
+# liczby3[3] = 2
+# print(liczby3)
+# liczby3.pop(2)
+# liczby3.remove(3)
+# print(liczby3)
+#
+# a = 'text'
+# lista1 = list(a)
+# print(lista1)
+# lista2 = [a]
+# print(lista2)
+
+# slownik = {'czerwony': 'red', 'niebeiski': 'blue', 'bialy': 'white'}
+# slowko = input("Podaj slowko do przetlumaczenia")
+# print(slownik[slowko])
+#
