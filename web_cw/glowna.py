d7098dbb997e7fa550a33613a5bcb5bbed61fa3e
Paulina <hryciuk.paulina1@gmail.com>
2024-10-04 18:49:02 +0200

    excercises

--- web_cw/glowna.py
+++ web_cw/glowna.py
@@ -8,26 +8,13 @@ def home():
         Go <a href="{url_for('notki')}">here</a> Notatki </br>'''
     return f'<h1 >Hello</h1></br>{menu}'
 
-@app.route('/notatki', methods= ['GET','POST'])
-def notki():
-    print (request.method)
-    if request.method == 'GET':
-        body = f'''
-                <form id="dodanie_notatki" action="{url_for('notki')}" method="POST">
-                <label for="nowa">NOWA</label>
-                <input type="text" 
-                <input type="text" id="amount" name="amount" value="100"><br>
-                <input type="submit" value="Send">
-                </form>
-                '''
-        return body
-    else:
-        return url_for('lista')
 
-@app.route('/lista')
-def lista():
-    return "LISTA NOTATEK"
-
+lista = ["pranie", "sprzatanie"]
+@app.route('/lista_zadan',methods= ['GET','POST'])
+def lista_zadan():
+    if request.method == 'POST':
+        lista.append(request.form['task'])
+    return render_template("lista_zadan.html",task=lista)
 
 
 if __name__=='__main__':
