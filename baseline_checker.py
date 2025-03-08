70758b6e405c4f8757097ddee45da901078a7b1d
Paulina <hryciuk.paulina1@gmail.com>
2025-03-08 07:21:25 +0100

    excercises

--- baseline_checker.py
+++ baseline_checker.py
@@ -1,6 +1,19 @@
 import xlwings as xw
 import openpyxl as xl
 
-def new_book:
-    wb = xw.Book()
-    sheet = wb.sheet[0]
+
+
+class output:
+    def __init__(self,avanterra):
+        self.avantera = avanterra
+
+
+    def new_book:
+        wb = xw.Book()
+        sheet = wb.sheet[0]
+
+
+class sources:
+    def __init__(self, avanterra, curr_monit):
+        self.avanterra = C:\Users\Paulina\PycharmProjects\Exercises\baseline\input
+        self.curr_monit = C:\Users\Paulina\PycharmProjects\Exercises\baseline\input
